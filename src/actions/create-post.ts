1cea366bc8547fef85cb96fae8d2cfb6ad712b7b
tomaCezar1 <cezar.toma1@gmail.com>
2026-02-27 10:17:18 +0200

    new create post server action

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,3 +1,55 @@
 'use server';
 
-export async function createPost() {}
+import { auth } from '@/auth';
+import { db } from '@/db';
+import { z } from 'zod';
+
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  slug: string,
+  formState: CreatePostFormState,
+  formData: FormData,
+): Promise<CreatePostFormState> {
+  console.log({ formState });
+  const result = createPostSchema.safeParse({
+    title: formData.get('title'),
+    content: formData.get('content'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user || !session.user.id) {
+    return {
+      errors: {
+        _form: ['Must be signed in'],
+      },
+    };
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { slug },
+  });
+
+  if (!topic) {
+    return { errors: { _form: ['Cannot find topic'] } };
+  }
+
+  return { errors: {} };
+}
