a6d3adbd0422009d5c8847555f272e33200dbbbc
tomaCezar1 <cezar.toma1@gmail.com>
2026-03-03 09:54:48 +0200

    create post list component

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import PostCreateForm from '@/app/components/posts/post-create-form';
+import PostList from '@/components/posts/post-list';
+import { fetchPostsByTopicSlug } from '@/db/queries/posts';
 
 interface TopicPageProps {
-  params: Promise<{
-    slug: string;
-  }>;
+  params: Promise<{ slug: string }>;
 }
 
 const TopicPage = async ({ params }: TopicPageProps) => {
@@ -13,6 +13,7 @@ const TopicPage = async ({ params }: TopicPageProps) => {
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostsByTopicSlug(slug)} />
       </div>
 
       <div>
