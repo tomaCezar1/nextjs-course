2e4f1db73a1f4de519b6c073912a250657ffdcc7
tomaCezar1 <cezar.toma1@gmail.com>
2026-03-05 09:58:13 +0200

    add skeleton component for posts

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,5 +1,31 @@
-const PostPage = () => {
-  return <div>Post Page Content</div>;
-};
+import Link from 'next/link';
+import PostShow from '@/components/posts/post-show';
+import CommentList from '@/components/comments/comment-list';
+import CommentCreateForm from '@/components/comments/comment-create-form';
+import paths from '@/paths';
+import { Suspense } from 'react';
+import PostShowLoading from '@/components/posts/post-show-loading';
 
-export default PostPage;
+interface PostShowPageProps {
+  params: Promise<{
+    slug: string;
+    postId: string;
+  }>;
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = await params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {'< '}Back to {slug}
+      </Link>
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList postId={postId} />
+    </div>
+  );
+}
