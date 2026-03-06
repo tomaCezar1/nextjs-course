4ba9bfa8777c0c5b2487d8dd12177ea6d254f916
tomaCezar1 <71145334+tomaCezar1@users.noreply.github.com>
2026-03-06 10:38:05 +0200

    create new fetchTopPosts server action

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -14,3 +14,20 @@ export function fetchPostsByTopicSlug(slug: string) {
     },
   });
 }
+
+export function fetchTopPosts(): Promise<PostWithData[]> {
+  return db.post.findMany({
+    orderBy: [
+      {
+        comments: {
+          _count: 'desc',
+        },
+      },
+    ],
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
