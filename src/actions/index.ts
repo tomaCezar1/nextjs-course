af87faee3d817cb5f6a3b6d6a08817f273285350
tomaCezar1 <71145334+tomaCezar1@users.noreply.github.com>
2026-03-10 11:02:46 +0200

    update next config

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,3 +3,4 @@ export { createTopic } from './create-topic';
 export { createPost } from './create-post';
 export { signIn } from './sign-in';
 export { signOut } from './sign-out';
+export { search } from './search';
