2767605a7ea845bd7ae7b4600dc3cde43459f913
tomaCezar1 <71145334+tomaCezar1@users.noreply.github.com>
2026-03-12 10:01:05 +0200

    create new posts action

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -31,3 +31,23 @@ export function fetchTopPosts(): Promise<PostWithData[]> {
     },
   });
 }
+
+export function FetchPostsBySearchTerm(term: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+    where: {
+      OR: [
+        {
+          title: { contains: term },
+        },
+        {
+          content: { contains: term },
+        },
+      ],
+    },
+  });
+}
