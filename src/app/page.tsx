f7e0870b063960eef9278e1f80adbbff081215b1
tomaCezar1 <71145334+tomaCezar1@users.noreply.github.com>
2026-04-08 10:20:22 +0300

    update app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import PostList from '@/components/posts/post-list';
 import TopicCreateForm from './components/topics/topic-create-form';
 import TopicList from './components/topics/topic-list';
+import { fetchTopPosts } from '@/db/queries/posts';
 
 export default async function Home() {
   // const session = await auth();
@@ -26,6 +28,7 @@ export default async function Home() {
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top Posts</h1>
+        <PostList fetchData={fetchTopPosts} />
       </div>
       <div>
         <TopicCreateForm />
